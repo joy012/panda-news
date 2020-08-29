a03b576434032c24260e58d740d154aeb8dca3fa
joy012 <joy.buet.012@gmail.com>
2020-08-30 00:00:46 +0600

    fragment error fixed

--- src/components/TopHeadLine/TopHeadLine.js
+++ src/components/TopHeadLine/TopHeadLine.js
@@ -11,14 +11,12 @@ const TopHeadLine = () => {
     }, [])
     return (
         // fragment <> </> do the same work as a div do in returning multiple tag together. but fragment doesn't create a extra div in the virtual dom
-        <>
+        <div>
             <h1 className="text-center">Top Headlines: {news.length}</h1>
-            <div className="news">
             {
                 news.map(article => <News article={article}></News>)
             }
-            </div>
-        </>
+        </div>
     );
 };
 
