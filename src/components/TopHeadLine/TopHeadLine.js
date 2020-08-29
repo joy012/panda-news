d8270d076c0c3aabeb6fa57e9e5104245974eee1
joy012 <joy.buet.012@gmail.com>
2020-08-30 00:09:40 +0600

    error fixed

--- src/components/TopHeadLine/TopHeadLine.js
+++ src/components/TopHeadLine/TopHeadLine.js
@@ -5,12 +5,11 @@ const TopHeadLine = () => {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        fetch('http://newsapi.org/v2/top-headlines?country=us&apiKey=85f4365db19845298708f4616c6fbdbc')
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=85f4365db19845298708f4616c6fbdbc')
         .then(response => response.json())
         .then(data => setNews(data.articles))
     }, [])
     return (
-        // fragment <> </> do the same work as a div do in returning multiple tag together. but fragment doesn't create a extra div in the virtual dom
         <div>
             <h1 className="text-center">Top Headlines: {news.length}</h1>
             {
